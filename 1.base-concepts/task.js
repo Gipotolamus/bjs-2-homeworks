f0b48173f6ae89e8c2e5f1b35c0f99e69dfaa2e8
Mirau Pavel <miraupavel@gmail.com>
2024-01-13 14:04:59 +0700

    First task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let x1;
+  let x2;
   
-  return arr;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [(-b + Math.sqrt(d))/(2*a)];
+  } else {
+    x1 = (-b + Math.sqrt(d) )/(2*a);
+    x2 = (-b - Math.sqrt(d) )/(2*a) ;
+    arr.push(x1, x2);
+  }
+  return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let per = parseInt(percent) / 100 / 12;
+	let con = parseInt(contribution);
+	let am = parseInt(amount);
+	if (isNaN(per) || per < 0) {
+		return `false`;
+	} else {
+			let s = am - con;
+			let n = countMonths;
+			let pay = s * (per + per / (((1 + per) ** n) - 1));
+			let totalAmount = (pay * n).toFixed(2);
+			console.log(totalAmount);
+			return +totalAmount;
+   }
 }
